71d3768a00a5673949656cf795dca8f48d08d010
Samuel de Morais <samucatnbpratic@gmail.com>
2020-12-24 08:58:56 -0300

    Projeto SalesWebMvc novo projeto

---
+++ SalesWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWevMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
