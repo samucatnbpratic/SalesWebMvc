f6cc99900725b0a6ee12c9c707dd30c4e80c50f3
Samuel de Morais <samucatnbpratic@gmail.com>
2020-12-30 16:01:02 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWevMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["email"] = "samuel@gmail.com";
 
             return View();
         }
