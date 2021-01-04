4757f0e38c8d74028e10160265ccc9674f0f1e40
Samuel de Morais <samucatnbpratic@gmail.com>
2021-01-04 17:29:38 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -44,6 +44,7 @@ namespace SalesWebMvc
             //registra nossos serviços no sistema de injeção de depencia da aplicação
             services.AddScoped<SeedingService>(); 
             services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
